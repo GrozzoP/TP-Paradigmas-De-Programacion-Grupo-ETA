865c1ed14dfe37b7517fcef38fa39c7ae8e09c1d
Grosso <95724294+GrozzoP@users.noreply.github.com>
2025-11-24 21:49:50 -0300

    Comment out SWI_HOME_DIR property in Main.java
    Comment out SWI_HOME_DIR property setting for debugging.

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        //System.setProperty("SWI_HOME_DIR", "C:\\Program Files\\swipl");
+        
         System.out.println("Iniciando aplicación de gestión de recitales...");
 
         Menu menuApp = new Menu();
