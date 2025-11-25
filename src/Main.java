205ec1d6629064148356c75fcc92a3783985ec07
Grosso <95724294+GrozzoP@users.noreply.github.com>
2025-11-24 21:48:01 -0300

    Remove SWI_HOME_DIR property setting

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.setProperty("SWI_HOME_DIR", "C:\\Program Files\\swipl");
         System.out.println("Iniciando aplicación de gestión de recitales...");
 
         Menu menuApp = new Menu();
